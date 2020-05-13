676fd6ad38860aa707c25fbf162fb6b5ef2ae5d3
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-12 23:05:11 -0400

    You can now search by date or amount

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { setTextFilter } from '../actions/filters'
+import { setTextFilter, sortByDate, sortByAmount } from '../actions/filters'
 
 const ExpenseListFilters = (props) => {
   return (
@@ -14,11 +14,15 @@ const ExpenseListFilters = (props) => {
       <select 
       value={props.filters.sortBy}
       onChange={(e) => {
-        
+        if (e.target.value === 'date') {
+          props.dispatch(sortByDate())
+        } else if (e.target.value === 'amount' ) {
+          props.dispatch(sortByAmount())
+        }
       }}
       >
-        <option>Date</option>
-        <option>Amount</option>
+        <option value="date">Date</option>
+        <option value="amount">Amount</option>
       </select>
     </div>
   )
