14ff2593f7313b0e9a503818366c8a4bec32ccf5
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-13 17:45:52 -0400

    Created edit expense and filters for project

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import Expenseform from './ExpenseForm'
+import { addExpense } from '../actions/expenses'
 
 
-const AddExpensePage = () => (
+const AddExpensePage = (props) => (
   <div>
     <h1>Add Expense</h1>
     <Expenseform 
       onSubmit={(expense) => {
-        console.log(expense)
+        props.dispatch(addExpense(expense))
+        props.history.push('/')
       }}
     />
   </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage)
