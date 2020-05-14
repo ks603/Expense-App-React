d5343ea062baa5f7168c49e76fe9623d5a425b15
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-13 22:15:59 -0400

    Changes to webpack for production

--- src/app.js
+++ src/app.js
@@ -8,18 +8,19 @@ import getVisibleExpenses from './selectors/expenses'
 import configureStore from './store/configureStore'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore()
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))
-store.dispatch(addExpense({ description: 'rent', amount: 1000000 }))
+// store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))
+// store.dispatch(addExpense({ description: 'rent', amount: 1000000 }))
 
-store.dispatch(addExpense({ description: 'gass bill', createdAt: 1000 }))
+// store.dispatch(addExpense({ description: 'gass bill', createdAt: 1000 }))
 
 
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses)
+// const state = store.getState()
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+// console.log(visibleExpenses)
 
 
 const jsx = (
